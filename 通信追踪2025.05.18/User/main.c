afb274d8a0f61f83629f27438ac36827429a18d0
Wikhh <140184043+Wikhh@users.noreply.github.com>
2026-05-14 17:28:29 +0800

    Update main.c

--- 通信追踪2025.05.18/User/main.c
+++ 通信追踪2025.05.18/User/main.c
@@ -1,30 +1,25 @@
 
 /******************************************************
-
-  ���ߣ�Laasckp	
-  ʱ�䣺2025.02.25
-  �汾��Car-32-4S_V4.7
-
-
-  ���ܣ�1.��·�������       
-        2.��·����ѭ��
-        3.�ٶȺ�����̲���
-        4.�����������ת����
-        5.����������
-        6.��ص�ѹ����
-        7.OLED��ʾ����ʾ
-        8.PWM�ٶȵ���
-        9.����ң�ؿ���
-       10.��׿����APP����(ѡ��)
+  功能：1.两路红外避障       
+        2.两路红外循迹
+        3.速度和总里程测量
+        4.超声波舵机旋转避障
+        5.蜂鸣器鸣叫
+        6.电池电压测量
+        7.OLED显示屏显示
+        8.PWM速度调节
+        9.红外遥控控制
+       10.安卓蓝牙APP控制(选配)
+	   11.接收声源信息寻迹
 
 
-  ���ܳ�����ο�interface.c
-  �ӿڶ��壺��ο�interface.h
+  汇总程序：请参考interface.c
+  接口定义：请参考interface.h
  			
 ******************************************************/
 
 
-#include "interface.h" //ģ��ӿڶ���ͷ�ļ�
+#include "interface.h" //模块接口定义头文件
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -36,29 +31,29 @@ int pack;
     int main(void)
  { 	 
 	
-	all_init();//��ʼ��������Ҫ�ĺ���	
+	all_init();//初始化所有想要的函数	
 	OLED_Clear();
 	 
     while(1)
   {	 
 
-//	 stop_choice();//������ʱִ�У�LED��˸�����ֹͣѡ������
+//	 stop_choice();//函数定时执行，LED闪烁，电机停止选择函数。
 //		
-//	 stop_display_execute(); //�����ͣ��ʾ����
+//	 stop_display_execute(); //电机暂停显示函数
 //		
-//	 speed_set_display_execute();//�ٶ�������ʾ��ִ�к���  
+//	 speed_set_display_execute();//速度设置显示和执行函数  
 //	    		  
-//	 search_display_execute(); //��·����ѭ����ʾ��ִ�к���
+//	 search_display_execute(); //两路红外循迹显示和执行函数
 //	  
-//	 obstacle_avoidance_display_execute();//���������ʾ��ִ�к���	
+//	 obstacle_avoidance_display_execute();//红外避障显示和执行函数	
 //	  
-//	 decode_choice();//����ң�أ��������ƣ�ѡ���ܺ���
+//	 decode_choice();//红外遥控，蓝牙控制，选择功能函数
 	  
 	  if (packet_ready)
 	  {
             packet_ready = 0;
             
-            // �����ַ�����������
+            // 解析字符串到浮点数
             received_angle = atoi(uart_rx_buffer);
 		    OLED_ShowCH(0,0,"angle=");   
 			OLED_ShowNum(56,0,received_angle,4,1);
